c51831f2330027ec15f31c4e4bbc467af16e4f5f
Anoushka Bhutani <abhutani@umich.edu>
2024-08-23 09:49:07 -0400

    package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 install_requires = open(path.join(here, "requirements.txt")).read().strip().split("\n")
 
 setup(
-    name="simple_flow_match",
+    name="simple_flow_matching",
     version="0.0.1",
     packages=find_packages(),
     install_requires=install_requires,
